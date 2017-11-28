8490b744156c2d3bcf851dda844987fc6e5c5c97
spedubois <spenser@mechdesign.com>
2017-11-28 14:32:04 -0700

    started implementation

--- app/src/main/java/com/example/spenserdubois/battleship/LoginActivity.kt
+++ app/src/main/java/com/example/spenserdubois/battleship/LoginActivity.kt
@@ -2,11 +2,28 @@ package com.example.spenserdubois.battleship
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.*
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+
+class LoginActivity : AppCompatActivity(){
+
+    private lateinit var btnReg : Button
+    private lateinit var textEmail : EditText
+    private lateinit var textPassword : EditText
+    private lateinit var textView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnReg = registerBtn
+        textEmail = editEmail
+        textPassword = editPassword
+        textView = textSignIn
     }
 }
